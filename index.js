06dbe319630bc1da6fef34917d224a9747293dd3
lhelina <hilinaashenafi38@gmail.com>
2025-10-16 16:10:24 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-// HINTS:
-// 1. Import express and axios
+
 import express from "express";
 import axios from "axios";
-// 2. Create an express app and set the port number.
+
 const app = express();
 const port = 3001;
 app.set("view engine", "ejs");
 
-// 3. Use the public folder for static files.
 app.use(express.static("public"));
-// 4. When the user goes to the home page it should render the index.ejs file.
+
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get(
@@ -23,10 +21,7 @@ app.get("/", async (req, res) => {
     res.status(500);
   }
 });
-// 5. Use axios to get a random secret and pass it to index.ejs to display the
-// secret and the username of the secret.
 
-// 6. Listen on your predefined port and start the server.
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
